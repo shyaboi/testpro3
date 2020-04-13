a6ca4f1a61654d266b422fb3c04f4dce37ac24bb
Ian <ian.shane.sears@gmail.com>
2020-04-12 21:14:53 -0700

    trying to make scroll to bottom on new submit changed the output a bit

--- src/components/chat/index.js
+++ src/components/chat/index.js
@@ -1,12 +1,23 @@
-import React from "react";
 import "./style.css";
-import ReactDOM from 'react-dom'
+import React, { Component, useRef, useEffect } from 'react';
+import { render } from 'react-dom';
+// import ReactDOM from 'react-dom'
 
 
 class Chat extends React.Component {
+    
     constructor(props) {
         super(props);
-
+      
+        const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)   
+        const useMountEffect = (fun) => useEffect(fun, [])
+        
+        const ScrollDemo = () => {
+            const myRef = useRef(null)
+        
+            useMountEffect(() => scrollToRef(myRef)) // Scroll on mountA
+        }
+        
         this.state = {
             value: '',
             toDoList: []
@@ -21,17 +32,21 @@ class Chat extends React.Component {
     }
 
     handleSubmit(event) {
-        if (this.state.value == '') {
+
+        if (this.state.value === '') {
             alert('Put some words in the box son')
             return
         }
+
         const itemsArray = this.state.value.split(',');
         this.setState({toDoList: itemsArray});
         var items = this.state.toDoList.concat(itemsArray);
         this.setState({toDoList: items})
-        window.scrollTo(0, document.body.scrollHeight);
+   
+
+
         event.preventDefault();
-        this.mainInput.value = ""
+        this.state.value = ""
     }
 
     render() {
@@ -40,12 +55,11 @@ class Chat extends React.Component {
         const items = this.state.toDoList.map((item) => <li>{item}</li>);
 
         return (
+            
             <form id='chat'
                 onSubmit={
                     this.handleSubmit
             }>
-                <ul id='k'>
-                    {items}</ul>
                 <label>
                     <input ref={(ref) => this.mainInput= ref} type="text"
                         value={
@@ -56,10 +70,13 @@ class Chat extends React.Component {
                         }/>
                 </label>
                 <input id='butt' type="submit" value="Chat"/>
+                <ul ref={this.myRef} id='k'>
+                    {items}</ul>
             </form>
         );
     }
 
 }
+
 
 export default Chat;
