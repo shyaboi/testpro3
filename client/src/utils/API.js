be8b188462a652f4026a1d1aa5bcf84589846b64
Justin <jlkellogg18@outlook.com>
2020-04-14 10:23:28 -0700

    public api

---
+++ client/src/utils/API.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export default {
+  getProjects: function () {
+    return axios.get("/api/projects");
+  },
+  getProject: function (id) {
+    return axios.get("/api/projects/" + id);
+  },
+  deleteProject: function (id) {
+    return axios.delete("/api/projects/" + id);
+  },
+  createProject: function (projectData) {
+    return axios.post("/api/projects/", projectData);
+  },
+
+  getTasks: function () {
+    return axios.get("/api/tasks");
+  },
+
+  getTask: function (id) {
+    return axios.get("/api/tasks/" + id);
+  },
+
+  deleteTasks: function (id) {
+    return axios.delete("/api/tasks/" + id);
+  },
+
+  createTask: function (taskData) {
+    return axios.post("/api/tasks" + taskData);
+  },
+
+  getDeadlines: function () {
+    return axios.get("/api/deadlines");
+  },
+
+  deleteDeadline: function (id) {
+    return axios.delete("/api/deadlines/" + id);
+  },
+};
